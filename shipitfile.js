a62346cb7f2bc3e8be8333d161885f082d83d791
yugo <belovedyogurt@gmail.com>
2017-12-16 15:31:03 +0800

    edit

--- shipitfile.js
+++ shipitfile.js
@@ -1,4 +1,6 @@
-module.exports = function (shipit) {
+'use strict';
+
+module.exports = function(shipit) {
   require('shipit-deploy')(shipit);
 
   shipit.initConfig({
@@ -6,28 +8,28 @@ module.exports = function (shipit) {
       workspace: '/tmp/github-monitor',
       deployTo: '/home/nono/app',
       repositoryUrl: 'https://github.com/MiYogurt/deploy-egg-sample.git',
-      ignores: ['.git', 'node_modules'],
-      rsync: ['--del'],
+      ignores: [ '.git', 'node_modules' ],
+      rsync: [ '--del' ],
       keepReleases: 2,
       key: './scripts/source.key',
-      shallowClone: true
+      shallowClone: true,
     },
     staging: {
-      servers: 'root@139.199.227.41'
-    }
+      servers: 'root@139.199.227.41',
+    },
   });
 
-  shipit.task('install', function () {
+  shipit.task('install', function() {
     shipit.remote('npm install --prod', {
-      cwd : '/home/nono/app/current'
+      cwd: '/home/nono/app/current',
     }).then(() => {
-      shipit.start('run')
-    })
+      shipit.start('run');
+    });
   });
 
-  shipit.task('run', function () {
+  shipit.task('run', function() {
     return shipit.remote('npm start', {
-      cwd : '/home/nono/app/current'
-    })
+      cwd: '/home/nono/app/current',
+    });
   });
-}
+};
