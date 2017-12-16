76d44a97dbc16ceb1f65f8227977efd238960e62
yugo <belovedyogurt@gmail.com>
2017-12-16 15:14:46 +0800

    add shipit

---
+++ shipitfile.js
@@ -0,0 +1,33 @@
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/github-monitor',
+      deployTo: '/home/nono/app',
+      repositoryUrl: 'https://github.com/MiYogurt/deploy-egg-sample.git',
+      ignores: ['.git', 'node_modules'],
+      rsync: ['--del'],
+      keepReleases: 2,
+      key: './scripts/source.key',
+      shallowClone: true
+    },
+    staging: {
+      servers: 'root@139.199.227.41'
+    }
+  });
+
+  shipit.task('install', function () {
+    shipit.remote('npm install --prod', {
+      cwd : '/home/nono/app/current'
+    }).then(() => {
+      shipit.start('run')
+    })
+  });
+
+  shipit.task('run', function () {
+    return shipit.remote('npm start', {
+      cwd : '/home/nono/app/current'
+    })
+  });
+}
